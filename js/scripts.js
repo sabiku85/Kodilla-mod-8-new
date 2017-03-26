e143b7ebc840d261815a11bf3c9b2e5cf0d30914
sabina <pilkauna@wp.pl>
2017-03-26 23:27:14 +0200

    Mod 8.5 If / else

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
-var a = 5,
-	h = 8,
-	triangleArea = a*h/2;
+var a = 2,
+	b = 5;
 
-console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+value = (a*a) + (2*a*b) - (b*b);
+console.log(value);
+
+// sprawdzenie: liczba jest dodatnia lub ujemna lub jest zerem
+if (value < 0) {
+	console.log("wynik ujemny");
+} else if (value > 0) {
+	console.log("wynik dodatni");
+} else {
+	console.log("wynik równa się zero")
+}
