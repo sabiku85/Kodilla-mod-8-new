12873ae220140ff843f870ffdd5349cebab0bdcc
sabina <pilkauna@wp.pl>
2017-03-26 23:38:42 +0200

    Mod 8.6 functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
-var a = 2,
-	b = 5;
-
-value = (a*a) + (2*a*b) - (b*b);
-console.log(value);
-
-// sprawdzenie: liczba jest dodatnia lub ujemna lub jest zerem
-if (value < 0) {
-	console.log("wynik ujemny");
-} else if (value > 0) {
-	console.log("wynik dodatni");
-} else {
-	console.log("wynik równa się zero")
+function getTriangleArea(a, h) {
+  if (a <= 0 || h <= 0) {
+    console.log('Nieprawidłowe dane');
+  } else {
+    return (a*h/2);
+  }
 }
+
+console.log(getTriangleArea(10,6));
+
+var triangleArea1 = getTriangleArea(5,12),
+	triangleArea2 = getTriangleArea(8,3),
+	triangleArea3 = getTriangleArea(2,9);
+
+console.log('Pole trójkąta nr 1 = ' + triangleArea1);
+console.log('Pole trójkąta nr 2 = ' + triangleArea2);
+console.log('Pole trójkąta nr 3 = ' + triangleArea3);
