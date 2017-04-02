54552f19fd4a1efc7d7bbacaa680306a49206aac
sabina <pilkauna@wp.pl>
2017-04-02 13:45:57 +0200

    Variable instead of DOM element (in loop)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var buttonClasses = document.getElementsByClassName('button');
 
 for (var k = 0; k < buttonClasses.length; k++) {
-	alert(document.getElementsByClassName('button')[k].innerText);
+	alert(buttonClasses[k].innerText);
 };
